839ce8d382f31db51c0cf752002c5e0de503e7c8
denysoncardoso <102764760+denysoncardoso@users.noreply.github.com>
2023-12-28 19:06:04 -0300

    adicionando rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
+import { CorteComponent } from './pages/corte/corte.component';
 import { authGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
@@ -23,6 +24,10 @@ const routes: Routes = [
     path: 'perfil',
     component: PerfilComponent,
     canActivate: [authGuard]
+  },
+  {
+    path: 'corte',
+    component: CorteComponent,
   }
 ];
 

1ef951e8bb09f4c6362703bc3e4220493eee1d92
denysoncardoso <102764760+denysoncardoso@users.noreply.github.com>
2023-12-28 18:52:49 -0300

    Adicionando nova pagina de lista de cortes

---
+++ src/app/pages/corte/corte.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-corte',
+  templateUrl: './corte.component.html',
+  styleUrls: ['./corte.component.scss']
+})
+export class CorteComponent {
+
+}
